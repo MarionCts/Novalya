108781fbf7e3265960e2fe9e46e783717c421845
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-09-12 12:00:11 +0200

    Adding modal windows for all delete buttons

---
+++ public/script/modal.js
@@ -0,0 +1,71 @@
+// MODAL WINDOWS
+
+// Fetching the elements for the Delete Modal
+
+const deleteButtons = document.querySelectorAll(".button__delete");
+const modal = document.getElementById("deleteConfirmationModal");
+const confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
+const cancelDeleteBtn = document.getElementById("cancelDeleteBtn");
+const modalTitle = document.getElementById("modalDeleteTitle");
+const modalMessage = document.getElementById("modalDeleteMessage");
+let formToSubmit = null;
+
+// Translations FR & EN
+
+const translations = {
+    fr: {
+        "modal.delete": "Confirmer la suppression",
+        "modal.message":
+            "Êtes-vous sûr de vouloir supprimer ? Cette action est irréversible.",
+    },
+    en: {
+        "modal.delete": "Confirm deletion",
+        "modal.message":
+            "Are you sure you want to delete? This action is irreversible.",
+    },
+};
+
+const locale = document.documentElement.dataset.lang || window.currentLocale || "fr";
+
+function translation(key) {
+
+    if (key) {
+    return translations[locale][key]; // locale will be either 'FR' or 'EN', and the key will be the parameter of the translation (= modal.delete)
+    } 
+}
+
+// Fetches all the existing delete buttons, and prevents the default behavior of the submit button for all
+deleteButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+        event.preventDefault(); 
+        formToSubmit = this.closest("form"); 
+
+        // Fetches the dataset ID of each button
+        const propertyId = this.dataset.propertyId;
+        const categoryId = this.dataset.categoryId;
+        const tagId = this.dataset.tagId;
+        const userId = this.dataset.userId;
+
+        // Writes the custom translations of the title and the message for each case (deletion, newsletter, etc)
+        if (propertyId || categoryId || tagId || userId) {
+            let title = translation("modal.delete");
+            modalTitle.textContent = title;
+            let message = translation("modal.message");
+            modalMessage.textContent = message;
+        }
+
+        modal.classList.remove("hides-modal");
+    });
+});
+
+confirmDeleteBtn.addEventListener("click", function () {
+    if (formToSubmit) {
+        formToSubmit.submit(); // Submits the form
+    }
+    modal.classList.add("hides-modal");
+});
+
+cancelDeleteBtn.addEventListener("click", function () {
+    modal.classList.add("hides-modal");
+    formToSubmit = null; // Resets the form
+});
