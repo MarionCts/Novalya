ac4cf1c163aeb0ccd9f8a665a682506ac5b7305e
Marion Cts <104251209+MarionCts@users.noreply.github.com>
2025-09-19 15:03:29 +0200

    Adding return to top button

--- public/script/utils.js
+++ public/script/utils.js
@@ -1,7 +1,19 @@
 // NEWSLETTER FORM
 
-const newsBtn = document.getElementById('newsBtn');
+const newsBtn = document.getElementById("newsBtn");
 
-newsBtn.addEventListener('click', function(event) {
+newsBtn.addEventListener("click", function (event) {
     event.preventDefault();
+});
+
+// RETURN TO TOP BUTTON SCROLLING/FADING EFFECT
+
+const returnBtn = document.getElementById("returnBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        returnBtn.classList.add("appears");
+    } else if (window.scrollY < 300) {
+        returnBtn.classList.remove("appears");
+    }
 });
